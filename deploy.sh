1a27ea69d79c8413dcc64e4f8289c7670c63fb0d
poizoni <wkphillips11@gmail.com>
2022-10-11 22:32:58 -0400

    leaflet maps implemented & some css improvements

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ cd dist
 
 git init
 git add -A 
-git commit -m 'V6 Accuracy Update Deployment'
+git commit -m 'V7 Map Update Deployment'
 git push -f git@github.com:Poizoni/ButhiGame.git master:gh-pages
 
 cd -
